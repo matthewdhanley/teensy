fbb0e698ab7edddf084fbaa149a4e681ece9bc6a
matthewdhanley <maha2895@colorado.edu>
2018-04-23 18:33:44 -0600

    more code

---
+++ ProportionalControl_matt/ProportionalControl_matt.ino
@@ -0,0 +1,46 @@
+//This is an example of how you would control 1 stepper
+
+#include <AccelStepper.h>
+
+int motorSpeed = 10000; //maximum steps per second (about 3rps / at 16 microsteps)
+int motorAccel = 8000; //steps/second/second to accelerate
+
+int motorDirPin = 2; //digital pin 2
+int motorStepPin = 3; //digital pin 3
+
+int motor_loc = 0;
+
+//set up the accelStepper intance
+//the "1" tells it we are using a driver
+AccelStepper stepper(1, motorStepPin, motorDirPin); 
+
+
+
+void setup(){
+  stepper.setMaxSpeed(motorSpeed);
+  stepper.setSpeed(motorSpeed);
+  stepper.setAcceleration(motorAccel);
+  
+//  stepper.moveTo(32000); //move 32000 steps (should be 10 rev)
+}
+
+void loop(){
+  
+  //if stepper is at desired location
+  if (Serial.available()){
+    motor_loc = Serial.parseInt();
+  }
+  
+  if (stepper.distanceToGo() == 0){
+    //go the other way the same amount of steps
+    //so if current position is 400 steps out, go position -400
+    stepper.moveTo(motor_loc); 
+  }
+  
+
+  
+  //these must be called as often as possible to ensure smooth operation
+  //any delay will cause jerky motion
+  stepper.run();
+}
+
